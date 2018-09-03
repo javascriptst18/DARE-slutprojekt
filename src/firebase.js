787a56020e50017d9471efde0430f34c629d6288
Gustav Strid <gustav.strid@gmail.com>
2018-09-03 11:50:13 +0200

    FIrst

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+//  Inside of firebase.js file
+import firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyD6uZ0MZvzpPU7AiiB0ImnWfagoBZqmlHU',
+  authDomain: 'dare-2dd84.firebaseapp.com',
+  databaseURL: 'https://dare-2dd84.firebaseio.com',
+  projectId: 'dare-2dd84',
+  storageBucket: 'dare-2dd84.appspot.com',
+  messagingSenderId: '665169555641',
+};
+
+firebase.initializeApp(config);
+export default firebase;
