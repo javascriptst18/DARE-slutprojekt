cfdd83161ef4fe06afff23f3d77b70ddbf18b3b7
Kajsa Lindholm <kajsamlindholm@gmail.com>
2018-09-04 12:10:16 +0200

    form jsx complete

--- src/components/newDareForm.js
+++ src/components/newDareForm.js
@@ -5,7 +5,10 @@ class NewDare extends Component {
     state = {
         place: '',
         radius:0,
-        date: 0,
+        date: '',
+        timeStart: '',
+        timeEnd: '',
+        duration: 0,
         budget: 0,
         level: 2, //needs some kind of explanation in UI
     }
@@ -18,32 +21,65 @@ class NewDare extends Component {
     render() {
         return(
             <form> 
-                <label htmlFor="level">Chicken or DAREDevil? 
+                <label htmlFor="level">
+                    Chicken or DAREDevil? 
                     <input 
                     type="range" 
+                    list="tickmarks"
                     min="1" 
                     max="3" 
                     value={this.state.daredevil} 
                     className="slider" 
                     id="level"
-                    onChange={this.onChange}
-                /> 
+                    onChange={this.onChange} />
                 </label>
-                <label htmlFor="place">Plats
+                <label htmlFor="place">
+                    Plats
                     <input 
                     type="text"
                     value={this.state.place}
                     onChange={this.onChange}/>
                 </label>
-                <label htmlFor="">Jag vill ha en aktivitet inom 
+                <label htmlFor="">
+                    Jag vill ha en aktivitet inom 
                     <input 
                     type="number"
                     id="radius"
                     value={this.state.radius}
                     onChange={this.onChange}/>
-                km
+                    km
                 </label>
-            
+                <label htmlFor="date"> 
+                    Datum
+                    <input 
+                    type="date" 
+                    id="date"
+                    onChange={this.onChange} />
+                </label>
+                <label htmlFor="timeStart"> 
+                    Starttid
+                    <input 
+                    type="time" 
+                    id="timeStart"
+                    onChange={this.onChange} />
+                </label>
+                <label htmlFor="timeEnd"> 
+                    Sluttid
+                    <input 
+                    type="time" 
+                    id="timeEnd"
+                    onChange={this.onChange} /> 
+                </label>
+                <label htmlFor="budget">
+                    Max budget
+                    <input 
+                    type="number"
+                    id="budget"
+                    onChange={this.onChange} />
+                </label>
+                <input
+                    type="submit"
+                    value="Skicka in" />
             </form>
         )
     }
