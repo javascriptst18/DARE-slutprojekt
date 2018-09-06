25cb2999fb902dca8602ed9387c0b15180523915
Johannes Ljungberg <johannesljungberg@Johanness-MacBook-Pro.local>
2018-09-06 14:13:09 +0200

    nyD

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'; //  for redux
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import Dares from './components/dares';
 import LogIn from './components/Login';
 import UserSettings from './components/UserSettings';
