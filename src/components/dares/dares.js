f9d929da3a4f89fc41f1b539a55818ae3df03064
Kajsa Lindholm <kajsamlindholm@gmail.com>
2018-09-10 16:31:40 +0200

    in the middle of checking what to display in dares aka not working

--- src/components/dares/dares.js
+++ src/components/dares/dares.js
@@ -2,12 +2,41 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import NewDare from './newDareForm';
 import UpcomingDare from './upcomingDare';
+import db from '../../firebase';
 
 
 class Dares extends Component {
 
     componentDidMount() {
-    
+        this.checkDaresDb();
+    }
+
+    checkDaresDb = () => {
+        let current = {};
+        db.collection('queue').doc(this.props.user.email).get()
+        .then((documentSnapshot) => {
+           if (documentSnapshot.exists) {
+           current = documentSnapshot.data;
+           console.log(current);
+           return current;
+           }
+            else console.log('ingen dare just nu')
+            } );
+        db.collection('userMatch').where('id1', '==', this.props.user.email).where('activity', '==', false).get()
+        .then((documentSnapshot) => {
+            if (documentSnapshot.exists) {
+            return current = documentSnapshot.data;
+            }
+             else console.log('ingen dareMatch1 just nu')
+             } );
+        db.collection('userMatch').where('id2', '==', this.props.user.email).where('activity', '==', false).get()
+        .then((documentSnapshot) => {
+            if (documentSnapshot.exists) {
+                return current = documentSnapshot.data;
+            }
+             else console.log('ingen dareMatch2 just nu')
+            });
+        
     }
 
     decline = () => {
